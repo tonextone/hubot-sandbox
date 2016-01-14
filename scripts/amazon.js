253c96eaf67d0c35e311583c0e6bea2c53d095a8
tonextone <master@tonextone.com>
2016-01-14 16:37:30 +0900

    fix

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -3,45 +3,45 @@ require('dotenv').config({silent: true});
 var amazon = require('amazon-product-api');
 
 module.exports = function(robot){
-  robot.hear(/^amazon (.*)/i, function(res){
-      var keyword = res.match[1];
-      var client = amazon.createClient({
-          awsId: process.env.AMAZON_AWS_ID,
-          awsSecret: process.env.AMAZON_AWS_SECRET,
-          awsTag: process.env.AMAZON_AWS_TAG
-      });
-      client.itemSearch({
-          keywords: keyword,
-          searchIndex: 'Books',
-          responseGroup: 'ItemAttributes',
-          domain: 'ecs.amazonaws.jp'
-      }, function(err, items){
-          if (err) {
-              console.log(err);
-              res.send('見つかりません');
-              return;
-          }
-          var message = '';
-          var num = items.length;
-          for (var i = 0; i < num; i++) {
-              if (i >= 1) {
-                  message += 'ほか'+(num-1)+'件';
-                  if (num >=10) message += '以上';
-                  break;
-              }
-              var item = items[i];
-              var title = item.ItemAttributes[0].Title;
-              var code = '';
-              if (item.ItemAttributes[0].EAN) {
-                  code += 'EAN: ' + item.ItemAttributes[0].EAN[0];
-              }
-              if (item.ASIN) {
-                  code += ', ASIN: ' + item.ASIN[0];
-              }
-              var url = item.DetailPageURL[0];
-              message += (title + '\n' + code + '\n' + url + '\n');
-          }
-          res.send(message);
-      });
-  });
+    robot.respond(/(az|amzn|amaz|amazon) (.*)/i, function(res){
+        var keyword = res.match[1];
+        var client = amazon.createClient({
+            awsId: process.env.AMAZON_AWS_ID,
+            awsSecret: process.env.AMAZON_AWS_SECRET,
+            awsTag: process.env.AMAZON_AWS_TAG
+        });
+        client.itemSearch({
+            keywords: keyword,
+            searchIndex: 'Books',
+            responseGroup: 'ItemAttributes',
+            domain: 'ecs.amazonaws.jp'
+        }, function(err, items){
+            if (err) {
+                console.log(err);
+                res.send('見つかりません');
+                return;
+            }
+            var message = '';
+            var num = items.length;
+            for (var i = 0; i < num; i++) {
+                if (i >= 1) {
+                    message += 'ほか'+(num-1)+'件';
+                    if (num >=10) message += '以上';
+                    break;
+                }
+                var item = items[i];
+                var title = item.ItemAttributes[0].Title;
+                var code = '';
+                if (item.ItemAttributes[0].EAN) {
+                    code += 'EAN: ' + item.ItemAttributes[0].EAN[0];
+                }
+                if (item.ASIN) {
+                    code += ', ASIN: ' + item.ASIN[0];
+                }
+                var url = item.DetailPageURL[0];
+                message += (title + '\n' + code + '\n' + url + '\n');
+            }
+            res.reply(message);
+        });
+    });
 };
