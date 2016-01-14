c22d2eeab183e9ac9dc52f21bf041b9dc594b48f
tonextone <master@tonextone.com>
2016-01-14 12:23:55 +0900

    save

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export HUBOT_TYPETALK_CLIENT_ID='vw4H2HtWAbqmq5LJIYHkelFznaUORu8F' ;
-export HUBOT_TYPETALK_CLIENT_SECRET='oFU8DtTYt0Wz6vadCbgyoCw9VRzXtZ6GzedjqIfpswpHEs5oLdS4jghHX8PzBgjP' ;
-export HUBOT_TYPETALK_ROOMS=20829 ;
-export HUBOT_TYPETALK_API_RATE=100 ;
-
-bin/hubot -a typetalk ;
