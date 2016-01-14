5099538ec67c8668c2274eae97d5feb6b09bd253
tonextone <master@tonextone.com>
2016-01-14 10:20:43 +0900

    sleep.

--- scripts/clock-king.js
+++ scripts/clock-king.js
@@ -2,6 +2,23 @@ var Job = require('cron').CronJob;
 var theRoom = (typeof(process.env)=='undefined') ? 20829 : process.env.HUBOT_TYPETALK_ROOMS ;
 
 module.exports = function(robot){
+    var cid = setInterval(function(){
+        if (typeof(robot.send) !== 'function') return;
+        robot.send({room: theRoom}, 'ガバリ');
+        clearInterval(cid);
+    }, 1000);
+    
+    var on_sigterm = function(){
+        robot.send({room: theRoom}, 'スヤリ');
+        setTimeout(process.exit, 1000);
+    };
+    
+    if (typeof(process._events.SIGTERM) !== 'undefined') {
+        process._events.SIGTERM = on_sigterm;
+    } else {
+        process.on('SIGTERM', on_sigterm);
+    }
+    
     var morning = new Job({
         cronTime: '0 0 9 * * 1-5',
         timeZone: 'Asia/Tokyo',
@@ -27,3 +44,4 @@ module.exports = function(robot){
         }
     });
 };
+
